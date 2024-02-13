a625735f8d0c9621e904b18463bcc32ba4ab34e2
豆浆° <39391923+SoyBeanMilkx@users.noreply.github.com>
2024-02-13 22:21:57 +0800

    Update main.c

--- main/main.c
+++ main/main.c
@@ -30,24 +30,20 @@
 #define CAM_PIN_PCLK 22
 #define CAM_PIN_DATA1 4 //闪光灯和DATA1共用
 
-//NAMESPACE
 #define NVS_NAMESPACE "Picture_Count"
 
-//KEY_DATA
 #define NVS_PICTURE_COUNT_KEY "picture_count"
 
-// 定义ESP32的SSID和密码
 #define ESP_SSID "Yangser"
 #define ESP_PASS "password"
 
-// 定义ESP32的IP地址和端口号
+IP地址和端口号
 #define ESP_IP "192.168.4.1"
 #define ESP_PORT 8080
 
 static const char *TAG = "img2img";
 static sdmmc_card_t *card;
 
-//配置camera
 static camera_config_t camera_config = {
         .pin_pwdn = CAM_PIN_PWDN,
         .pin_reset = CAM_PIN_RESET,
@@ -67,21 +63,19 @@ static camera_config_t camera_config = {
         .pin_href = CAM_PIN_HREF,
         .pin_pclk = CAM_PIN_PCLK,
 
-        //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
         .xclk_freq_hz = 20000000,
         .ledc_timer = LEDC_TIMER_0,
         .ledc_channel = LEDC_CHANNEL_0,
 
-        .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
-        .frame_size = FRAMESIZE_240X240,    //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
+        .pixel_format = PIXFORMAT_JPEG,
+        .frame_size = FRAMESIZE_240X240,
 
-        .jpeg_quality = 20, //0-63, for OV series camera sensors, lower number means higher quality
-        .fb_count = 2,       //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
+        .jpeg_quality = 20,
+        .fb_count = 2, 
         .grab_mode = CAMERA_GRAB_LATEST,
         .fb_location = CAMERA_FB_IN_DRAM,
 };
 
-//初始化相机
 static esp_err_t init_camera(void) {
     //initialize the camera
     esp_err_t err = esp_camera_init(&camera_config);
@@ -113,21 +107,13 @@ static esp_err_t init_sd_card() {
     return ret;
 }*/
 
-// 初始化ESP32的AP模式
 void init_ap() {
-
     nvs_flash_init();
-
     esp_netif_init();
-
     esp_event_loop_create_default();
-
     esp_netif_t *ap = esp_netif_create_default_wifi_ap();
-
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-
     esp_wifi_init(&cfg);
-
     esp_wifi_set_mode(WIFI_MODE_AP);
 
     wifi_config_t ap_config = {
@@ -142,108 +128,78 @@ void init_ap() {
 
     esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_config);
 
-    // 创建一个ip信息结构体
     esp_netif_ip_info_t ip_info;
 
-    // 设置ip信息结构体的参数
-    IP4_ADDR(&ip_info.ip, 192, 168, 4, 1); // 设置ip地址
-    IP4_ADDR(&ip_info.netmask, 255, 255, 255, 0); // 设置子网掩码
-    IP4_ADDR(&ip_info.gw, 192, 168, 4, 1); // 设置网关
+    IP4_ADDR(&ip_info.ip, 192, 168, 4, 1);
+    IP4_ADDR(&ip_info.netmask, 255, 255, 255, 0);
+    IP4_ADDR(&ip_info.gw, 192, 168, 4, 1);
 
     esp_netif_dhcps_stop(NULL);
-
     esp_netif_set_ip_info(ap, &ip_info);
-
     esp_netif_dhcps_start(NULL);
 
     esp_wifi_start();
 }
 
-// 创建一个TCP服务器socket并监听客户端连接请求
 int create_server_socket() {
-    // 创建一个socket描述符
     int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
 
-    // 检查socket是否创建成功
     if (server_socket < 0) {
         ESP_LOGE(TAG, "Failed to create socket\n");
         return -1;
     }
 
-    // 创建一个地址结构体
     struct sockaddr_in server_addr;
 
-    // 设置地址结构体的参数
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(ESP_PORT);
     server_addr.sin_addr.s_addr = inet_addr(ESP_IP);
 
-    // 绑定socket和地址
     int bind_result = bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
 
-    // 检查绑定是否成功
     if (bind_result < 0) {
         ESP_LOGE(TAG, "Failed to bind socket\n");
         return -1;
     }
 
-    // 监听socket
     int listen_result = listen(server_socket, 5);
 
-    // 检查监听是否成功
     if (listen_result < 0) {
         ESP_LOGE(TAG, "Failed to listen socket\n");
         return -1;
     }
 
-    // 返回socket描述符
     return server_socket;
 }
 
-// 接收消息
 bool receive_message(int client_socket) {
-    // 创建一个缓冲区
     char buffer[10];
-
-    // 接收客户端发送的数据
     int recv_len = recv(client_socket, buffer, sizeof(buffer), 0);
 
-    // 检查接收是否成功
     if (recv_len < 0) {
         ESP_LOGE(TAG, "Failed to receive data\n");
         return false;
     }
-
-    // 打印接收到的数据
     ESP_LOGI(TAG, "Received: %s\n", buffer);
     return true;
 }
 
-// 发送消息
 void send_message(int client_socket, const void *data, size_t data_len) {
-    // 发送数据长度信息
     send(client_socket, &data_len, sizeof(size_t), 0);
-
-    // 发送二进制数据
     int send_len = send(client_socket, data, data_len, 0);
-
-    // 检查发送是否成功
+        
     if (send_len < 0) {
         ESP_LOGE(TAG, "Failed to send data\n");
         return;
     }
-
-    // 检查是否完全发送了数据
     if (send_len != data_len)
         ESP_LOGW(TAG, "Incomplete data sent: %zd out of %zu bytes\n", send_len, data_len);
     else
         ESP_LOGI(TAG, "数据发送成功\n");
 
-    // 打印发送的数据长度
     ESP_LOGI(TAG, "Sent data of length: %zu\n", data_len);
 }
 
-// 拍摄保存并发送图像
 esp_err_t send_write_image(int client_socket) {
 
     /*char picture_name[29];
@@ -251,7 +207,6 @@ esp_err_t send_write_image(int client_socket) {
 
     ESP_LOGI(TAG, "Picture Count: %s", picture_name);*/
 
-    // 配置GPIO为输出模式
     gpio_config_t io_conf = {
             .pin_bit_mask = (1ULL << CAM_PIN_DATA1),
             .mode = GPIO_MODE_OUTPUT,
@@ -266,7 +221,7 @@ esp_err_t send_write_image(int client_socket) {
     vTaskDelay(pdMS_TO_TICKS(20));
     gpio_set_level(CAM_PIN_DATA1, 0);
 
-    vTaskDelay(pdMS_TO_TICKS(50)); //没测试，感觉可有可无
+    vTaskDelay(pdMS_TO_TICKS(50));
 
     //init_sd_card(); //DATA1使用完必须初始化sd卡，否则无法访问
 
@@ -295,7 +250,7 @@ esp_err_t send_write_image(int client_socket) {
     ESP_LOGI(TAG, "Write successed");*/
 
     send_message(client_socket, pic->buf, pic->len);
-    //vTaskDelay(pdMS_TO_TICKS(10));
+    //vTaskDelay(pdMS_TO_TICKS(10)); 加了后端会报错，不知道为什么
 
     //fclose(file);
     esp_camera_fb_return(pic);
@@ -309,14 +264,9 @@ esp_err_t send_write_image(int client_socket) {
 void app_main(void) {
     if (ESP_OK != init_camera() || ESP_OK != init_nvs())
         return;
-
-    // 初始化AP模式
     init_ap();
-
-    // 创建一个服务器socket
     int server_socket = -1;
 
-    // 循环尝试创建socket，直到成功
     while (server_socket < 0) {
         server_socket = create_server_socket();
         if (server_socket < 0)
@@ -327,36 +277,25 @@ void app_main(void) {
     bool img_sent = false;
 
     while (true) {
-        // 创建一个地址结构体
         struct sockaddr_in client_addr;
-
-        // 创建一个地址长度变量
         socklen_t addr_len = sizeof(client_addr);
-
-        // 接受客户端的连接请求
         int client_socket = accept(server_socket, (struct sockaddr *) &client_addr, &addr_len);
-
-        // 检查客户端socket是否创建成功
+            
         if (client_socket < 0)
             ESP_LOGE(TAG, "Failed to accept client socket\n");
 
-        // 打印客户端的IP地址和端口号
         ESP_LOGI(TAG, "Client connected from %s:%d\n", inet_ntoa(client_addr.sin_addr),
                  ntohs(client_addr.sin_port));
 
         if (!img_sent) {
-            // 发送图片
             send_write_image(client_socket);
             img_sent = true;
         }
         vTaskDelay(pdMS_TO_TICKS(3000));
 
-        // 接收消息并处理任务
         if (receive_message(client_socket)) {
-            // 关闭客户端socket
             close(client_socket);
             img_sent = false;
-            // 打印客户端已断开连接的信息
             ESP_LOGI(TAG, "Client disconnected\n");
         }
 
